4ba086b33efb12370f6560966f2b937ed4f6c4b3
Sai-9676 <sainathreddyguraka@gmail.com>
2024-12-28 22:38:05 +0530

    updated minor changes

--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CrudService {
-  private apiUrl = 'http://localhost:3000/api/users'; // Replace with your API endpoint
+  private apiUrl = 'http://54.179.17.14:4000/api/users'; // Replace with your API endpoint
 
   constructor(private http: HttpClient) {}
 
